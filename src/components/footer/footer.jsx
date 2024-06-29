befa501b289830b1b8d0f34b7cfd70f5bbf1bbc2
Bhuvan-Hegde <bhuvanshegde@gmail.com>
2024-06-29 10:55:28 +0530

    qol updates

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from 'footerStyles.module.css';
+
+
+export default function footer() {
+  return (
+    <>
+        <span className={styles.mainFooter}>
+        
+        </span>
+    
+    
+    </>
+  )
+}
