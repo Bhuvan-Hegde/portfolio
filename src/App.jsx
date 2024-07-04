6ca2c05f44e4906b648f48b91e1e29bf73a8dc29
Bhuvan-Hegde <bhuvanshegde@gmail.com>
2024-07-04 19:34:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import Hero from './components/Hero/Hero'
 import Projects from './components/projects/Projects'
-import Footer from './components/Footer/Footer'
+import foot from './components/Footer/Footer'
 
 function App() {
 
@@ -11,6 +11,7 @@ function App() {
     <>
       <Hero />
       <Projects />
+      <foot />
     
     
     </>
